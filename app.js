b6ae46bd78aa183c6c559b3ab04ca37dd8d468b7
Tehminanaz <tn0457584@gmail.com>
2024-04-02 15:33:22 -0700

    second commit

---
+++ app.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPin = 3179;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your pin",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("correct pin code");
+    let oprationAns = await inquirer.prompt([
+        {
+            name: "opration",
+            type: "list",
+            message: "select one option",
+            choices: ["withdraw", "check balance"],
+        }
+    ]);
+    console.log(oprationAns);
+    if (oprationAns.opration === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                type: "number",
+                message: "Enter your amount",
+            }
+        ]);
+        myBalance -= amountAns.amount;
+        console.log("your remaining balance is:" + myBalance);
+    }
+    else if (oprationAns.opration === "check balance") {
+        console.log(`"your balance is" ${myBalance}`);
+    }
+}
+else {
+    console.log("incorrect pin number");
+}
